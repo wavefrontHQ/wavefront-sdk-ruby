5d691a71f544e4fa691010fb2026558ee67cf551
yogeshprasad <yogeshprasadkurmi@gmail.com>
2019-01-28 18:33:45 +0530

    testting

--- common/atomic_integer.rb
+++ common/atomic_integer.rb
@@ -1,16 +1,17 @@
 # An atomic, thread-safe incrementing counter.
 #
 # @author Yogesh Prasad Kurmi (ykurmi@vmware.com)
+module Wavefront
+  class AtomicInteger
+    def initialize
+      @value = 0
+      @lock = Mutex.new
+    end
 
-class AtomicInteger
-  def initialize
-    @value = 0
-    @lock = Mutex.new
-  end
-
-  def increment(num=1)
-    @lock.synchronize do
-      @value += num
+    def increment(num=1)
+      @lock.synchronize do
+        @value += num
+      end
     end
   end
 end
--- common/proxy_connection_handler.rb
+++ common/proxy_connection_handler.rb
@@ -8,51 +8,53 @@ require_relative 'atomic_integer'
 
 # Connection Handler.
 # For sending data to a Wavefront proxy listening on a given port.
-class ProxyConnectionHandler
-  # Construct ProxyConnectionHandler.
-  # @param address [String] Proxy Address
-  # @param port [Integer] Proxy Port
-  def initialize(address, port)
-    @failures = AtomicInteger.new
-    @address = address
-    @port = port
-    @reconnecting_socket = nil
-  end
+module Wavefront
+  class ProxyConnectionHandler
+    # Construct ProxyConnectionHandler.
+    # @param address [String] Proxy Address
+    # @param port [Integer] Proxy Port
+    def initialize(address, port)
+      @failures = AtomicInteger.new
+      @address = address
+      @port = port
+      @reconnecting_socket = nil
+    end
 
-  # Open a socket connection to the given address:port
-  def connect
-    @reconnecting_socket = TCPSocket.open(@address, @port)
-  end
+    # Open a socket connection to the given address:port
+    def connect
+      @reconnecting_socket = TCPSocket.open(@address, @port)
+    end
 
-  # Close socket if it's open now.
-  def close
-    @reconnecting_socket.close if @reconnecting_socket
-  end
+    # Close socket if it's open now.
+    def close
+      @reconnecting_socket.close if @reconnecting_socket
+    end
 
-  def failure_count
-    @failures.value
-  end
+    def failure_count
+      @failures.value
+    end
 
-  def increment_failure_count
-    @failures.increment
-  end
+    def increment_failure_count
+      @failures.increment
+    end
 
-  # Send data via proxy.
+    # Send data via proxy.
 
-  # @param line_data [String] Data to be sent
-  # @param reconnect [Boolean] If it's the second time trying to send data
-  def send_data(line_data, reconnect=true)
-    begin
-      connect unless @reconnecting_socket
-      @reconnecting_socket.puts(line_data.encode('utf-8'))
-    rescue SocketError => error
-      if reconnect
-        @reconnecting_socket = nil
-        # Try to resend
-        send_data(line_data, false)
-      else
-        # Second time trying failed
-        raise error
+    # @param line_data [String] Data to be sent
+    # @param reconnect [Boolean] If it's the second time trying to send data
+    def send_data(line_data, reconnect=true)
+      begin
+        connect unless @reconnecting_socket
+        @reconnecting_socket.puts(line_data.encode('utf-8'))
+      rescue SocketError => error
+        if reconnect
+          @reconnecting_socket = nil
+          # Try to resend
+          send_data(line_data, false)
+        else
+          # Second time trying failed
+          raise error
+        end
       end
     end
   end
